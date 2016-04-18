d6bc177328010d45405f8398063783cc13ea630b
Vinay Kola <vinaykola@gmail.com>
2016-04-17 19:21:56 -0700

    Pelican update and images for new theme

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://vinaykola.com'
+SITEURL = 'http://heartofgold-42.appspot.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
